02c9aeae5179e578d0bb6ff884ce9e485f3b873e
RJ Li <ruijli@lm-sfo-11002531.corp.ebay.com>
2016-08-11 16:09:26 -0700

    Finished database design

--- SafewayBearsontheGo-Django/bearwalk/serializers.py
+++ SafewayBearsontheGo-Django/bearwalk/serializers.py
@@ -1,1 +1,19 @@
 from rest_framework import serializers
+from bearwalk.models import User,Group
+
+class GroupSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    current_location = serializers.CharField(required=False, allow_blank=True, max_length=200)
+    destination = serializers.CharField(required=False, allow_blank=True, max_length=200)
+    users = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Group
+        fields = ('current_location', 'destination', 'users')
+
+class UserSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(required=False, allow_blank=True, max_length=20)
+    password = serializers.CharField(required=False, allow_blank=True, max_length=20)
+    current_location = serializers.CharField(required=False, allow_blank=True, max_length=200)
+    destination = serializers.CharField(required=False, allow_blank=True, max_length=200)
--- SafewayBearsontheGo-Django/bearwalk/views.py
+++ SafewayBearsontheGo-Django/bearwalk/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from bearwalk.models import User,Group
+from bearwalk.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def login(request):
+    serializer = UserSerializer(username=request.data.username, password=request.data.password)
